497f05236c10248a44c7eaad4a0b9c330f370f8c
AntonioVGTU <anton20060956@mail.ru>
2025-03-10 03:08:16 +0300

    Add files via upload

---
+++ lab 5/lab 5 prot.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+using namespace std;
+
+class point {
+public:
+	int x, y, a;
+	void zero() {
+		cout << "обнуление x и y ";
+		x = 0;
+		y = 0;
+		cout << "значение x " << x << "\nзначение y " << y << endl;
+	}
+	
+	void range() {
+		double rast;
+		rast = sqrt(x + y);
+		cout << "расстояние от начала кординат до точнки = " << rast << endl;
+	}
+};
+void main() {
+	setlocale(LC_ALL, "ru");
+	cout << "выберите действие\n" << "1. ввод значений для x и y\n" << "2. обнулить значения x и y\n" << "3. расчитать растояние от начала кординатной плоскости до точки" << endl;
+	int choise;
+	cin >> choise;
+	point tocka;
+	switch (choise)
+	{
+	case 1: 
+		cin >> tocka.x;
+		cin >> tocka.y;
+		cout << "значение x " << tocka.x << "\nзначение y " << tocka.y << endl;
+	case 2:
+		tocka.zero();
+	case 3:
+		tocka.range();
+	}
+
+}
